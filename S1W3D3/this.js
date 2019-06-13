18f42d9d50ec08ff8a5dae68faebe305979c43f8
patpalmerston <patpalmerston@gmail.com>
2019-06-13 08:01:45 -0700

    solution

--- S1W3D3/this.js
+++ S1W3D3/this.js
@@ -1,15 +1,23 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window/Global Object Binding - the window object or global object binds all global parameters to the global this.
+
+* 2. Implicit Binding - Implicit binding is symbolized by the dot notation and the preceding parameter of the dot is the implicit this that calls on on the parameter after the dot.
+
+* 3. New Binding
+
+* 4. Explicit Binding
+
 *
 * write out a code example of each explanation above
 */
 
 // Principle 1
+  const word = function(word) {
+    console.log(this)
+    return word
+  }
 
 // code example for Window Binding
 
